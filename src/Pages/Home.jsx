a1afb6fa5acbec00b3539f05aab8d19070a5de2e
vijay Dhakad <dhakadvijay2684@gmail.com>
2024-12-20 23:09:38 +0530

    Your commit message

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../Components/Header/Header'
+import SpecialityMenu from '../Components/SpecialityMenu/SpecialityMenu'
+import TopDoctors from '../Components/TopDoctors/TopDoctors'
+import Banner from '../Components/Banner/Banner'
+
+const Home = () => {
+  return (
+    <div>
+     <Header/>
+     <SpecialityMenu/>
+     <TopDoctors/>
+     <Banner/>
+    </div>
+  )
+}
+
+export default Home
